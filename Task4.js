16b8732278d585d2777498426319b2500ef66a13
Denzeltmn <denzeltmn@gmail.com>
2024-12-16 20:09:16 +0500

    Задание 8 (twoSum)

--- Task4.js
+++ Task4.js
@@ -1,3 +1,19 @@
-Вход: nums = [2,7,11,15], target = 9
-Вывод: [0,1]
-Объяснение: Поскольку nums[0] + nums[1] == 9, мы возвращаем [0, 1].
+function twoSum(nums, target)
+{
+    for (let i = 0; i < nums.length; i++)
+    {
+        for (let j = i + 1; j < nums.length; j++)
+        {
+            if (nums[i] + nums[j] === target)
+            {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+const nums = [2,7,11,15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result); //[0,1]
