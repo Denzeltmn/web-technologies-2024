8debb6fd29d04f9a7c375ff7008874d81ad7934d
Denzeltmn <denzeltmn@gmail.com>
2024-12-17 02:17:08 +0500

    Задание 8 (LongestLine)

--- Task5.js
+++ Task5.js
@@ -1,5 +1,32 @@
-Ввод: strs = ["цветок","поток","хлопок"]
-Вывод: "ок"
+var LongestLine = function (strs)
+{
+    if (strs.length === 0) return "";
+    function LongestLine(sub)
+    {
+        return strs.every(str => str.toLowerCase().includes(sub.toLowerCase()));
+    }
+    let longest = "";
+    for (let length = 2; length <= strs[0].length; length++)
+    {
+        for (let start = 0; start <= strs[0].length - length; start++)
+        {
+            let line = strs[0].slice(start, start + length);
+            if (LongestLine(line))
+            {
+                if (line.length > longest.length)
+                {
+                    longest = line;
+                }
+            }
+        }
+    }
+    return longest;
+};
 
-Ввод: strs = ["собака","гоночная машина","машина"]
-Вывод: ""
+const strs1 = ["цветок", "поток", "хлопок"];
+const strs2 = ["собака", "гоночная машина", "машина"];
+
+console.log(LongestLine(strs1)); // "ок"
+console.log(LongestLine(strs2)); // ""
+
+
