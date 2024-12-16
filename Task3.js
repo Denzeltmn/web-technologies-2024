401de3c759680b3d758df25f62f1e44b8b7da311
Denzeltmn <denzeltmn@gmail.com>
2024-12-16 19:48:10 +0500

    Задание 8 (spinWords)

--- Task3.js
+++ Task3.js
@@ -1,3 +1,17 @@
+function spinWords(str)
+{
+    const words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++)
+    {
+        if (words[i].length >= 5)
+        {
+            words[i] = words[i].split('').reverse().join('');
+        }
+    }
+    return words.join(' ');
+}
+
 const result1 = spinWords( "Привет от Legacy" )
 console.log(result1) // тевирП от ycageL
 
