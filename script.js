b6ddb94a579966d4bd9737a9dc0210ce63ec6578
Denzeltmn <denzeltmn@gmail.com>
2024-12-17 23:33:52 +0500

    Задание 9 (папочки красивые)

--- script.js
+++ script.js
@@ -1,124 +1,129 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init)
-} else {
-    init()
+if (document.readyState === 'loading')
+{
+    document.addEventListener('DOMContentLoaded', init);
 }
 
-function init() {
-    const data = {
-        name: 'Каталог товаров',
-        hasChildren: true,
-        items: [
-            {
-                name: 'Мойки',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran1',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT1',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT2',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Ulgran2',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT3',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT4',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            },{
-                name: 'Фильтры',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran3',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT5',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT6',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+else
+{
+    init();
+}
 
+function init()
+{
+    const data =
+        {
+            name: 'Каталог товаров',
+            hasChildren: true,
+            items: [
+                {
+                    name: 'Мойки',
+                    hasChildren: true,
+                    items: [
+                        {
+                            name: 'Ulgran',
+                            hasChildren: true,
+                            items: [
+                                { name: 'Smth', hasChildren: false, items: [] },
+                                { name: 'Smth', hasChildren: false, items: [] }
+                            ]
+                        },
+                        {
+                            name: 'Vigro Mramor',
+                            hasChildren: false,
+                            items: []
+                        },
+                        {
+                            name: 'Handmade',
+                            hasChildren: true,
+                            items: [
+                                { name: 'Smth', hasChildren: false, items: [] },
+                                { name: 'Smth', hasChildren: false, items: [] }
+                            ]
+                        },
+                        {
+                            name: 'Vigro Glass',
+                            hasChildren: false,
+                            items: []
+                        }
+                    ]
+                },
+                {
+                    name: 'Фильтры',
+                    hasChildren: true,
+                    items: [
+                        {
+                            name: 'Ulgran',
+                            hasChildren: true,
+                            items: [
+                                { name: 'Smth', hasChildren: false, items: [] },
+                                { name: 'Smth', hasChildren: false, items: [] }
+                            ]
+                        },
+                        {
+                            name: 'Vigro Mramor',
+                            hasChildren: false,
+                            items: []
+                        }
+                    ]
+                }
+            ]
+        };
 
-    const items = new ListItems(document.getElementById('list-items'), data)
+    const items = new ListItems(document.getElementById('list-items'), data);
+    items.render();
+    items.init();
 
-
-  /*  items.render()*/
-    items.init()
-
-    /*console.log(items.renderTest(data));*/
-
-    function ListItems(el, data) {
+    function ListItems(el, data)
+    {
         this.el = el;
         this.data = data;
 
-        this.init = function () {
-            const parents = this.el.querySelectorAll('[data-parent]')
+        this.init = function ()
+        {
+            this.el.addEventListener('click', (event) =>
+            {
+                const arrow = event.target.closest('[data-open]');
+                if (arrow)
+                {
+                    const parent = arrow.closest('[data-parent]');
+                    this.toggleItems(parent);
+                }
+            });
+        };
 
-            parents.forEach(parent => {
-                const open = parent.querySelector('[data-open]')
+        this.render = function ()
+        {
+            this.el.innerHTML = this.renderParent(this.data);
+        };
 
-                open.addEventListener('click', () => this.toggleItems(parent) )
-            })
-        }
+        this.renderParent = function (data)
+        {
+            let html = `
+                <div class="list-item" data-parent>
+                    <div class="list-item__inner">
+                        <div class="list-item__arrow-container">
+                            ${data.hasChildren ? '<img class="list-item__arrow" src="/img/chevron-down.png" alt="chevron-down" data-open>' : ''}
+                        </div>
+                        <img class="list-item__folder" src="/img/folder.png" alt="folder">
+                        <span class="list-item__text">${data.name}</span>
+                    </div>`;
 
-        this.render = function () {
-            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
-        }
+            if (data.hasChildren)
+            {
+                html += '<div class="list-item__items">';
+                data.items.forEach(item =>
+                {
+                    html += this.renderParent(item);
+                });
+                html += '</div>';
+            }
+            html += '</div>';
+            return html;
+        };
 
-        this.renderParent = function (data) {
-            //проверка всех элементов на hasChildren
-            //если hasChildren, то запускаем renderParent
-            //если !hasChildren, то запускаем renderChildren
-            //возвращает рендер родительского элемента
-
-        }
-
-        this.renderChildren = function (data) {
-            //вовзращает рендер элемента без вложенности
-        }
-
-        this.toggleItems = function (parent) {
-            parent.classList.toggle('list-item_open')
-        }
-
-/*        this.renderTest = function (data) {
-            return `
-            <div class="test">${data.name}</div>
-            `
-        }*/
+        this.toggleItems = function (parent)
+        {
+            parent.classList.toggle('list-item_open');
+        };
     }
-
 }
